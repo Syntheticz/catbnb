248ae054168b85beb612541bc418be8bea6e404a
Syntheticz <guiangphilip08@gmail.com>
2022-08-04 23:58:59 +0800

    Header

---
+++ component/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import style from '../styles/Header.module.css'
+import logo from '../images/logo.svg'
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <div className={style.header}>
+        <span className={style.logoImage}>
+            <Image src={logo} layoyt='fill' objectFit='contain' width='100%' height='100%'/>
+        </span> 
+        <a href="#" className={style.logoTitle}><h3 >Genesis</h3></a>
+        <div className={style.linksContainer}>
+            <Link href="#"><a>Home</a></Link>
+            <Link href="#"><a>Pricing</a></Link>
+            <Link href="#"><a>About</a></Link>
+        </div>
+    </div>
+  )
+}
